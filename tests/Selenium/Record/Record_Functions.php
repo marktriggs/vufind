9e893310d0ed92185acc01b3471bf4986cd85208
demiankatz <demiankatz@c33641aa-f933-0410-a13b-8402795397c9>
2011-03-22 13:14:08 +0000

    Improved Selenium integration tests (thanks to Preetha Rao).
    git-svn-id: https://vufind.svn.sourceforge.net/svnroot/vufind/trunk@3772 c33641aa-f933-0410-a13b-8402795397c9

---
+++ tests/Selenium/Record/Record_Functions.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ * Integration testing of Record module.
+ *
+ * PHP version 5
+ *
+ * Copyright (C) Villanova University 2010.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @category VuFind
+ * @package  Tests
+ * @author   Preetha Rao <vufind-tech@lists.sourceforge.net>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org/wiki/unit_tests Wiki
+ */
+
+require_once dirname(__FILE__) . '/../lib/SeleniumTestCase.php';
+//error_reporting(E_ALL);
+
+/**
+ * Integration testing of Record module.
+ *
+ * @category VuFind
+ * @package  Tests
+ * @author   Preetha Rao <vufind-tech@lists.sourceforge.net>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org/wiki/unit_tests Wiki
+ */
+class Record_Functions extends SeleniumTestCase
+{
+    /**
+     * Test various features on the record view page.
+     *
+     * @return void
+     * @access public
+     */
+    public function testRecord()
+    {
+        $this->open($this->baseUrl);
+        $this->assertRegExp("/Search Home/", $this->getTitle());
+        $this->click("link=A - General Works");
+        $this->waitForPageToLoad("$this->timeout");
+        $this->click(
+            "link=Journal of rational emotive therapy : " .
+            "the journal of the Institute for Rational-Emotive Therapy."
+        );
+
+        $this->waitForPageToLoad("$this->timeout");
+
+        /* Test the Cite This functionality */
+        //echo "Testing Cite This Functionality";
+        $this->click("link=Cite this");
+        $this->waitForElementPresent("id=popupboxContent");
+
+        //echo "\nAsserting if Cite This Lightbox was opened";
+        $this->assertElementPresent("id=popupboxContent");          //Asserting if Lightbox was opened
+
+        //echo "\nAsserting Lightbox Contents";
+        $this->verifyTextPresent($this->apa_text);
+        $this->verifyTextPresent("Institute for Rational-Emotive Therapy (New York, N. (1983). Journal of rational emotive therapy: The journal of the Institute for Rational-Emotive Therapy. [New York]: The Institute. ");
+        $this->verifyTextPresent($this->mla_text);
+        $this->verifyTextPresent("Institute for Rational-Emotive Therapy (New York, N.Y.). Journal of Rational Emotive Therapy: The Journal of the Institute for Rational-Emotive Therapy. [New York]: The Institute, 1983. ");
+        $this->verifyTextPresent("Warning: These citations may not always be 100% accurate.");
+
+        $this->click("link=close");                     // Close the light box
+        //echo "\nAsserting if Lightbox was closed\n";
+        $this->assertNotVisible("id=popupbox");                 // Assert that the lightbox is not visible
+
+        /* Test the Text This functionlaity */
+        //echo "\nTesting Text This Functionality";
+        $this->click("link=Text this");
+        $this->waitForElementPresent("id=popupboxContent");
+
+        //echo "\nAsserting if Text This Lightbox was opened";
+        $this->assertElementPresent("id=popupboxContent");          //Asserting if Lightbox was opened
+
+        //echo "\nSubmitting without any data";
+        //$this->type("name=to","2155467886");
+        $this->click("name=submit");
+
+        //echo "\nAsserting Error message";
+        $jsstring = "{selenium.browserbot.getCurrentWindow().document.getElementById('popupbox').innerHTML != " . "\"<h3>" . $this->sms_sending . "</h3>\"" . ";}" ;
+        $this->waitForCondition($jsstring, "$this->timeout");
+        $this->verifyTextPresent($this->sms_failure);
+        //$this->click("link=close");                       // Close the light box ( no close button cuurently )
+        //echo "\nAsserting if Lightbox was closed\n";
+        //$this->assertNotVisible("id=popupbox");                   // Assert that the lightbox is not visible
+
+        /* Test the Email This functionlaity  */
+        //echo "\n\nTesting Email This Functionality";
+        $this->click("link=Email this");
+        $this->click("link=Email this");
+        $this->waitForElementPresent("id=popupboxContent");
+
+        //echo "\nAsserting if Text This Lightbox was opened";
+        //$this->assertElementPresent("id=popupboxContent");            //Asserting if Lightbox was opened
+
+        //echo "\nSubmitting without any data";
+        $this->click("name=submit");
+
+        //echo "\nAsserting Error message";
+        $jsstring = "{selenium.browserbot.getCurrentWindow().document.getElementById('popupMessages').innerHTML != " . "\"<h3>" . $this->email_sending . "</h3>\"" . ";}" ;
+        $this->waitForCondition($jsstring, "$this->timeout");
+        $this->isTextPresent(preg_replace('/\s\s+/', ' ', $this->email_failure . ": " . $this->email_rcpt_err));
+
+        $this->click("link=close");                     // Close the light box
+        //echo "\nAsserting if Lightbox was closed\n";
+        $this->assertNotVisible("id=popupbox");                 // Assert that the lightbox is not visible
+    }
+}
+?>
+
--- tests/Selenium/lib/SeleniumTestCase.php
+++ tests/Selenium/lib/SeleniumTestCase.php
@@ -30,11 +30,7 @@
  * Set up test environment.
  */
 require_once dirname(__FILE__) . '/../../web/prepend.inc.php';
-
-/**
- * Load Selenium driver code.
- */
-require_once 'Testing/Selenium.php';
+require_once 'PHPUnit/Extensions/SeleniumTestCase.php';
 
 /**
  * Base class for building Selenium tests.
@@ -45,32 +41,26 @@ require_once 'Testing/Selenium.php';
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     http://vufind.org/wiki/unit_tests Wiki
  */
-class SeleniumTestCase extends PHPUnit_Framework_TestCase
+class SeleniumTestCase extends PHPUnit_Extensions_SeleniumTestCase
 {
-    protected $baseUrl;
-    protected $hostname;
+    protected $baseUrl;             // Base Url in our case http://localhost/vufind/"
+    protected $hostname;            // name of the host on which application runs
     protected $selenium;
+    protected $timeout = 30000;     // Default value of timeout is 30000ms ( 30 sec )
 
-    /**
-     * Constructor
-     *
-     * @access public
-     */
-    public function __construct()
-    {
-        // Load the default configuration, but override the base path so that
-        // Smarty can find the real templates and cache folders.
-        $configArray = parse_ini_file(
-            dirname(__FILE__) . '/../../../web/conf/config.ini', true
-        );
+    /* Variables read from ini files */
+    protected $sms_sending;         // string for sms sending message
+    protected $sms_failure;         // string for sms error message
+    protected $sms_success;         // string for sms success message
 
-        // Read base URL from config file:
-        $this->baseUrl = $configArray['Site']['url'];
+    protected $email_sending;       // string for email sending message
+    protected $email_failure;       // string for email failure message
+    protected $email_success;       // string for email success message
+    protected $email_rcpt_err;      // string for email recipient error
+    protected $email_sndr_err;      // string for email sender error
 
-        // Extract hostname from base URL:
-        preg_match('/https?:\/\/([^\/]*).*/', $this->baseUrl, $matches);
-        $this->hostname = $matches[1];
-    }
+    protected $apa_text;            // APA citation text title
+    protected $mla_test;            // MLA citation text title
 
     /**
      * Standard setup method.
@@ -80,8 +70,49 @@ class SeleniumTestCase extends PHPUnit_Framework_TestCase
      */
     public function setUp()
     {
-        $this->selenium = new Testing_Selenium("*firefox", $this->baseUrl);
-        $this->selenium->start();
+        //echo "\nCalling Setup\n";
+        $configArray = parse_ini_file(
+            dirname(__FILE__) . '/../../../web/conf/config.ini', true
+        );
+
+        // create the ini file name based on the langauge of the site.
+        $langfile =  dirname(__FILE__) . '/../../../web/lang/' .
+            $configArray['Site']['language'] . '.ini';
+        $iniArray = parse_ini_file($langfile, false);
+
+        // Read base URL from config file:
+        $this->baseUrl = $configArray['Site']['url'];
+
+        // Extract hostname from base URL:
+        preg_match('/https?:\/\/([^\/]*).*/', $this->baseUrl, $matches);
+        $this->hostname = $matches[1];
+
+        // Extract the variables from en.ini file
+        $this->sms_sending = $iniArray['sms_sending'];
+        $this->sms_failure = preg_replace('/\s\s+/', ' ', $iniArray['sms_failure']);
+        $this->sms_success = preg_replace('/\s\s+/', ' ', $iniArray['sms_success']);
+
+        $this->email_sending  = preg_replace(
+            '/\s\s+/', ' ', $iniArray['email_sending']
+        );
+        $this->email_failure  = preg_replace(
+            '/\s\s+/', ' ', $iniArray['email_failure']
+        );
+        $this->email_success  = preg_replace(
+            '/\s\s+/', ' ', $iniArray['email_success']
+        );
+        $this->email_rcpt_err = preg_replace(
+            '/\s\s+/', ' ', $iniArray['Invalid Recipient Email Address']
+        );
+        $this->email_sndr_err = preg_replace(
+            '/\s\s+/', ' ', $iniArray['Invalid Sender Email Address']
+        );
+
+        $this->apa_text    = preg_replace('/\s\s+/', ' ', $iniArray['APA Citation']);
+        $this->mla_text    = preg_replace('/\s\s+/', ' ', $iniArray['MLA Citation']);
+
+        $this->setBrowser('*firefox');
+        $this->setBrowserUrl($this->baseUrl);
     }
 
     /**
@@ -92,7 +123,7 @@ class SeleniumTestCase extends PHPUnit_Framework_TestCase
      */
     public function tearDown()
     {
-        $this->selenium->stop();
+        $this->stop();
     }
 }
 ?>
