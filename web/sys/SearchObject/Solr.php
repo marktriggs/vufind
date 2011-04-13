marktriggs/vufind

commit 04ae738d13f8edf3965f327f435ab6ed38d5043e

Fix for VUFIND-397 (quotes in author names caused problems in Author/Home action).
git-svn-id: https://vufind.svn.sourceforge.net/svnroot/vufind/trunk@3837 c33641aa-f933-0410-a13b-8402795397c9

web/sys/SearchObject/Solr.php
@@ -245,12 +245,12 @@ class SearchObject_Solr extends SearchObject_Base
                 $this->searchSubType = 'home';
                 // Remove our empty basic search (default)
                 $this->searchTerms = array();
-                // Prepare the search as a normal author search
-                $unquotedAuthor = str_replace('"', '', $_REQUEST['author']);
+                // Prepare the search as a normal author search (with escaped quotes)
+                $escapedAuthor = str_replace('"', '\"', $_REQUEST['author']);
                 $this->searchTerms[] = array(
                     'index'   => 'Author',
                     // Force phrase search for improved accuracy:
-                    'lookfor' => '"' . $unquotedAuthor . '"'
+                    'lookfor' => '"' . $escapedAuthor . '"'
                 );
             }
 

commit 77cd8630337665af345f51fbc5576f49658965fe

Fixed paging bug introduced by r3837.
git-svn-id: https://vufind.svn.sourceforge.net/svnroot/vufind/trunk@3843 c33641aa-f933-0410-a13b-8402795397c9

web/sys/SearchObject/Solr.php
@@ -755,8 +755,10 @@ class SearchObject_Solr extends SearchObject_Base
         // Author Home screen
         case "author":
             if ($this->searchSubType == 'home') {
+                // Reverse query manipulation from init() for consistent paging:
+                $term = str_replace('\"', '"', $this->searchTerms[0]['lookfor']);
                 $params[] = "author="  .
-                    urlencode($this->searchTerms[0]['lookfor']);
+                    urlencode(substr($term, 1, strlen($term) - 2));
             }
             if ($this->searchSubType == 'search') {
                 $params[] = "lookfor=" .