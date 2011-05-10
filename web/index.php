marktriggs/vufind

commit 1cacaf08a368305014262a3bb898125c68f9d778

Resolved VUFIND-173 (Allow variable page size in search results listings) based on patch from Eoghan Ó Carragáin.  Note that there is a minor but significant change to the index.php processing -- we now call $interface->initGlobals() after setting up the environment but before dispatching actions in order to allow a hook for setting global template values like remembered user limit preferences.
git-svn-id: https://vufind.svn.sourceforge.net/svnroot/vufind/trunk@3880 c33641aa-f933-0410-a13b-8402795397c9

web/index.php
@@ -145,10 +145,8 @@ $loggedOutModule = isset($configArray['Site']['defaultModule'])
 $module = ($user = UserAccount::isLoggedIn()) ? $loggedInModule : $loggedOutModule;
 $module = (isset($_GET['module'])) ? $_GET['module'] : $module;
 $module = preg_replace('/[^\w]/', '', $module);
-$interface->assign('module', $module);
 $action = (isset($_GET['action'])) ? $_GET['action'] : 'Home';
 $action = preg_replace('/[^\w]/', '', $action);
-$interface->assign('action', $action);
 
 // Process Authentication
 if (!$user) {
@@ -169,7 +167,9 @@ if (!$user) {
         }
     }
 }
-$interface->assign('user', $user);
+
+// Assign global interface values now that the environment is all set up:
+$interface->initGlobals();
 
 // Process Login Followup
 if (isset($_REQUEST['followup'])) {

web/conf/searches.ini
@@ -12,6 +12,16 @@ default_sort         = relevance
 ; should be one of the options present in the [Views] section below.
 default_view         = list
 
+; This section controls the result limit options for search results. default_limit
+; sets the default number of results per page. limit_options is a comma-separated
+; list of numbers to be presented to the end-user. If only one limit is required,
+; set default_limit and leave limit_options commented out.
+; WARNING: using large limits (i.e. greater than 20) may interfere with some book
+; preview options (i.e. HathiTrust) and may require you to raise your PHP memory
+; limits to avoid errors.
+default_limit        = 20
+;limit_options        = 10,20,40,60,80,100
+
 ; If this setting is true, boolean operators in searches (AND/OR/NOT) will only
 ; be recognized if they are ALL UPPERCASE.  If set to false, they will be
 ; recognized regardless of case.

web/interface/themes/blueprint/Author/home.tpl
@@ -40,7 +40,21 @@
         {foreach from=$viewList item=viewData key=viewLabel}
           {if !$viewData.selected}<a href="{$viewData.viewUrl|escape}" title="{translate text='Switch view to'} {translate text=$viewData.desc}" >{/if}<img src="{$path}/images/view_{$viewData.viewType}.png" {if $viewData.selected}title="{translate text=$viewData.desc} {translate text='view already selected'}"{/if}/>{if !$viewData.selected}</a>{/if}
         {/foreach}
-      {/if}</div>
+      {/if}
+      </div>
+      <div class="limitSelect"> 
+        {if $limitList|@count gt 1}
+          <form action="{$path}/Search/LimitResults" method="post">
+            <label for="limit">{translate text='Results per page'}</label>
+            <select id="limit" name="limit" onChange="document.location.href = this.options[this.selectedIndex].value;">
+              {foreach from=$limitList item=limitData key=limitLabel}
+                <option value="{$limitData.limitUrl|escape}"{if $limitData.selected} selected="selected"{/if}>{$limitData.desc|escape}</option>
+              {/foreach}
+            </select>
+            <noscript><input type="submit" value="{translate text="Set"}" /></noscript>
+          </form>
+        {/if}
+      </div>
       <form action="{$path}/Search/SortResults" method="post">
         <label for="sort_options_1">{translate text='Sort'}</label>
         <select id="sort_options_1" name="sort" class="jumpMenu">

web/interface/themes/blueprint/Search/advanced.tpl
@@ -99,6 +99,21 @@
           {/foreach}
         </fieldset>
       {/if}
+      {if $limitList|@count gt 1}
+        <fieldset class="span-4">
+          <legend>{translate text='Results per page'}</legend>
+          <select id="limit" name="limit">
+            {foreach from=$limitList item=limitData key=limitLabel}
+              {* If a previous limit was used, make that the default; otherwise, use the "default default" *}
+              {if $lastLimit}
+                <option value="{$limitData.desc|escape}"{if $limitData.desc == $lastLimit} selected="selected"{/if}>{$limitData.desc|escape}</option>
+              {else}
+                <option value="{$limitData.desc|escape}"{if $limitData.selected} selected="selected"{/if}>{$limitData.desc|escape}</option>
+              {/if}
+            {/foreach}
+          </select>
+        </fieldset>
+      {/if}
       {if $dateRangeLimit}
         {* Load the publication date slider UI widget *}
         {js filename="pubdate_slider.js"}

web/interface/themes/blueprint/Search/list.tpl
@@ -33,7 +33,21 @@
         {foreach from=$viewList item=viewData key=viewLabel}
           {if !$viewData.selected}<a href="{$viewData.viewUrl|escape}" title="{translate text='Switch view to'} {translate text=$viewData.desc}" >{/if}<img src="{$path}/images/view_{$viewData.viewType}.png" {if $viewData.selected}title="{translate text=$viewData.desc} {translate text='view already selected'}"{/if}/>{if !$viewData.selected}</a>{/if}
         {/foreach}
-      {/if}</div>
+      {/if}
+      </div>
+      <div class="limitSelect"> 
+        {if $limitList|@count gt 1}
+          <form action="{$path}/Search/LimitResults" method="post">
+            <label for="limit">{translate text='Results per page'}</label>
+            <select id="limit" name="limit" onChange="document.location.href = this.options[this.selectedIndex].value;">
+              {foreach from=$limitList item=limitData key=limitLabel}
+                <option value="{$limitData.limitUrl|escape}"{if $limitData.selected} selected="selected"{/if}>{$limitData.desc|escape}</option>
+              {/foreach}
+            </select>
+            <noscript><input type="submit" value="{translate text="Set"}" /></noscript>
+          </form>
+        {/if}
+      </div>
       <form action="{$path}/Search/SortResults" method="post">
         <label for="sort_options_1">{translate text='Sort'}</label>
         <select id="sort_options_1" name="sort" class="jumpMenu">

web/interface/themes/blueprint/Search/newitem-list.tpl
@@ -22,15 +22,28 @@
       </div>
 
       <div class="span-5 last">
-        <form action="{$path}/Search/SortResults" method="post">
-          {translate text='Sort'}
-          <select name="sort" class="jumpMenu">
-            {foreach from=$sortList item=sortData key=sortLabel}
-              <option value="{$sortData.sortUrl|escape}"{if $sortData.selected} selected="selected"{/if}>{translate text=$sortData.desc}</option>
-            {/foreach}
-          </select>
-          <noscript><input type="submit" value="{translate text="Set"}" /></noscript>
-        </form>
+        <div class="limitSelect"> 
+          {if $limitList|@count gt 1}
+            <form action="{$path}/Search/LimitResults" method="post">
+              <label for="limit">{translate text='Results per page'}</label>
+              <select id="limit" name="limit" onChange="document.location.href = this.options[this.selectedIndex].value;">
+                {foreach from=$limitList item=limitData key=limitLabel}
+                  <option value="{$limitData.limitUrl|escape}"{if $limitData.selected} selected="selected"{/if}>{$limitData.desc|escape}</option>
+                {/foreach}
+              </select>
+              <noscript><input type="submit" value="{translate text="Set"}" /></noscript>
+            </form>
+          {/if}
+          <form action="{$path}/Search/SortResults" method="post">
+            <label for="sort_options_1">{translate text='Sort'}</label>
+            <select id="sort_options_1" name="sort" class="jumpMenu">
+              {foreach from=$sortList item=sortData key=sortLabel}
+                <option value="{$sortData.sortUrl|escape}"{if $sortData.selected} selected="selected"{/if}>{translate text=$sortData.desc}</option>
+              {/foreach}
+            </select>
+            <noscript><input type="submit" value="{translate text="Set"}" /></noscript>
+          </form>
+        </div>
       </div>
       <div class="clear"></div>
     </div>

web/interface/themes/blueprint/Search/reserves-list.tpl
@@ -20,15 +20,28 @@
     </div>
   
     <div class="span-5 last">
-      <form action="{$path}/Search/SortResults" method="post">
-        {translate text='Sort'}
-        <select name="sort" class="jumpMenu">
-          {foreach from=$sortList item=sortData key=sortLabel}
-            <option value="{$sortData.sortUrl|escape}"{if $sortData.selected} selected="selected"{/if}>{translate text=$sortData.desc}</option>
-          {/foreach}
-        </select>
-        <noscript><input type="submit" value="{translate text="Set"}" /></noscript>
-      </form>
+      <div class="limitSelect"> 
+        {if $limitList|@count gt 1}
+          <form action="{$path}/Search/LimitResults" method="post">
+            <label for="limit">{translate text='Results per page'}</label>
+            <select id="limit" name="limit" onChange="document.location.href = this.options[this.selectedIndex].value;">
+              {foreach from=$limitList item=limitData key=limitLabel}
+                <option value="{$limitData.limitUrl|escape}"{if $limitData.selected} selected="selected"{/if}>{$limitData.desc|escape}</option>
+              {/foreach}
+            </select>
+            <noscript><input type="submit" value="{translate text="Set"}" /></noscript>
+          </form>
+        {/if}
+        <form action="{$path}/Search/SortResults" method="post">
+          <label for="sort_options_1">{translate text='Sort'}</label>
+          <select id="sort_options_1" name="sort" class="jumpMenu">
+            {foreach from=$sortList item=sortData key=sortLabel}
+              <option value="{$sortData.sortUrl|escape}"{if $sortData.selected} selected="selected"{/if}>{translate text=$sortData.desc}</option>
+            {/foreach}
+          </select>
+          <noscript><input type="submit" value="{translate text="Set"}" /></noscript>
+        </form>
+      </div>
     </div>
     <div class="clear"></div>
   </div>

web/interface/themes/blueprint/Search/searchbox.tpl
@@ -51,6 +51,8 @@
           </div>
         </div>
       {/if}
+      {* Load hidden limit preference from Session *}
+      {if $lastLimit}<input type="hidden" name="limit" value="{$lastLimit|escape}" />{/if}
     </form>
     <script type="text/javascript">$("#searchForm_lookfor").focus()</script>
   {/if}

commit 8b357beb8d90e40d8a7f83a7cd1169091ee1aef5

Fixed bug introduced in r3880 -- bad login information now correctly redisplays the form.
git-svn-id: https://vufind.svn.sourceforge.net/svnroot/vufind/trunk@3897 c33641aa-f933-0410-a13b-8402795397c9

web/index.php
@@ -161,6 +161,7 @@ if (!$user) {
     if ($shibLoginNeeded || $standardLoginNeeded) {
         $user = UserAccount::login();
         if (PEAR::isError($user)) {
+            $interface->initGlobals();
             include_once 'services/MyResearch/Login.php';
             Login::launch($user->getMessage());
             exit();