0ce5fb66c73d85e82c73284739cfe7486e57e32f
tuanyorku <tuanyorku@c33641aa-f933-0410-a13b-8402795397c9>
2011-10-15 17:31:33 +0000

    fixed util/index_reserves.php to use standard readConfig() to read config file instead
    git-svn-id: https://vufind.svn.sourceforge.net/svnroot/vufind/trunk@4370 c33641aa-f933-0410-a13b-8402795397c9

--- util/index_reserves.php
+++ util/index_reserves.php
@@ -35,7 +35,7 @@ require_once 'util.inc.php';
 require_once 'sys/ConnectionManager.php';
 
 // Read Config file
-$configArray = parse_ini_file(dirname(__FILE__) . '/../web/conf/config.ini', true);
+$configArray = readConfig();
 
 // Setup Solr Connection
 $solr = ConnectionManager::connectToIndex('SolrReserves');
